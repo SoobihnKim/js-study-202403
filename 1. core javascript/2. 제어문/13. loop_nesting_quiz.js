e4115f49fa42d9c8c33f93effc277e5addcaa2a1
SoobihnKim <ksbsj92@naver.com>
2024-03-21 13:57:48 +0900

    #반복문

---
+++ 1. core javascript/2. 제어문/13. loop_nesting_quiz.js
@@ -0,0 +1,15 @@
+
+// var a = +prompt('한 변(1)');
+// var b = +prompt('한 변(2)');
+
+var a = 3;
+var b = 4;
+
+var result = '';
+
+// for (var i = 1; i <= 3; i++) {
+//   console.log(`result ${}`);
+//   for(var j = 1; j <= 4; j++) {
+//     console.log(`*\n`);
+//   }
+// }
