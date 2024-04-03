cd3ec8698b95a19c164b5970e6e85561827873bb
soo <ksbsj92@naver.com>
2024-04-03 17:45:53 +0900

    #

--- 2. web-API/CALCULATOR/js/vendor.js
+++ 2. web-API/CALCULATOR/js/vendor.js
@@ -77,6 +77,6 @@ const calculate = () => {
       $currentCalculation.textContent = descriptionLog;
 
       // 로그 이력 쌓기
-      accumulateLog();
+      accumulateLog({descriptionLog, currentResult});
      
 };
