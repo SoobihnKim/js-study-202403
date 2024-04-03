a83ddb838549c10b4361525a3f483bc3205f6c52
soo <ksbsj92@naver.com>
2024-04-03 15:54:52 +0900

    #CALCULATOR

---
+++ 2. web-API/CALCULATOP/js/app.js
@@ -0,0 +1,17 @@
+
+// 실행코드 (함수호출, 이벤트 등록, 핸들러 정의,..)
+
+// 이벤트 핸들러 바인딩
+$addBtn.addEventListener('click', e => {
+  // 연산 처리 함수
+  calculate();
+});
+$subtractBtn.addEventListener('click', e => {
+  calculate();
+});
+$multiplyBtn.addEventListener('click', e => {
+  calculate();
+});
+$divideBtn.addEventListener('click', e => {
+  calculate();
+});
