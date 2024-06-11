6fe9bb806a4c5e4f3b31f461c2bab9d61b00eaea
soo <ksbsj92@naver.com>
2024-06-11 12:14:44 +0900

    # 컴포넌트 분리

--- 4. OOP/js/Components/ProductItem.js
+++ 4. OOP/js/Components/ProductItem.js
@@ -1,31 +1,43 @@
+import Component from "./Component.js";
 
-// li 태그 만드는 역할
-class ProductItem {
+// LI 태그 렌더링 담당
+class ProductItem extends Component {
 
-  constructor(prod) {
+  constructor(tagId, prod) {
+    super(tagId);
     this.prod = prod;
   }
 
   render() {
 
-    const {title, price, image, desc} =  this.prod;
+    const { title, price, image, desc } = this.prod;
 
-    const $prodLi = document.createElement('li');
-    $prodLi.classList.add('product-item');
-    $prodLi.innerHTML = `
-    <div>
-      <img src="${image}" alt="${title}">
-      <div class="product-item__content">
-        <h2>${title}</h2>
-        <h3>${price}원</h3>
-        <p>${desc}</p>
-        <button>담기</button>
+    // const $prodLi = document.createElement('li');
+    // $prodLi.classList.add('product-item');
+
+    const childHtml =`
+      <div>
+        <img src="${image}" alt="${title}">
+        <div class="product-item__content">
+          <h2>${title}</h2>
+          <h3>${price}원</h3>
+          <p>${desc}</p>
+          <button>담기</button>
+        </div>
       </div>
-    </div>
-  `;
+    `;
+
+    const $prodLi = this.createElement('li', 'product-item', childHtml);
+
+
+    const $btn = $prodLi.querySelector('button');
+    $btn.addEventListener('click', e => {
+      console.log(title);
+      console.log('버튼 크릭!!');
+    });
 
     return $prodLi;
   }
-};
+}
 
 export default ProductItem;
